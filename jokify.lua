3ec9e4ea3a43ec63fbba8785dd169e0e1c6b25cc
Josefine Klintberg <josefine.klintberg@hotmail.com>
2020-05-14 17:04:30 +0200

    Add basic functionality for addon

---
+++ jokify.lua
@@ -0,0 +1,12 @@
+jokify = { }
+
+function jokify:HelloWorld()
+    message("-->FUNNY JOKE HERE<--")
+end
+
+function jokify:HideGryphons()
+    MainMenuBarLeftEndCap:Hide()
+    MainMenuBarRightEndCap:Hide()
+end
+
+jokify:HelloWorld()
